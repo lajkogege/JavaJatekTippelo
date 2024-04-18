4d38e769b614becdd011976018588bb786f40797
Lajkó Gege <127200655+lajkogege@users.noreply.github.com>
2024-04-18 11:20:13 +0200

    Update JatekIndito.java

--- src/main/JatekIndito.java
+++ src/main/JatekIndito.java
@@ -1,21 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package main;
 
 public class JatekIndito {
-
-    public static void main(String[] args) {
-        /*referencia nélkül hivjuk az objektum metodusát*/
-       //new Jatek().start();
-       
-       /*Referenciát adunk az objektumra, és azon keresztül hivjuk*/
-       Jatek referencia= new Jatek();
-       referencia.start();
-       
-       /*Csak a kkonstrukort hívjuk*/
-       //new Jatek();
-    }
     
+    public static void main(String[] args) {
+        /* nincs referencia az objektumra: */
+        //new Jatek().start();
+        
+        /* adunk referenciát az objektumra: */
+        //Jatek referencia = new Jatek();
+        /* a referencián keresztül elérjük az objektum tagfüggvényeit: */
+        //referencia.start();
+        
+        /* nincs referencia, csak az objektum konstruktorát hívjuk: */
+        new Jatek();
+        
+////      3 játékos tippel
+//        new Jatekos().tippel();
+//        new Jatekos().tippel();
+//        new Jatekos().tippel();
+//        HIBA
+////      1 játékos tippel 3-szor, ez nem jó
+//        Jatekos j=new Jatekos();
+//        j.tippel();
+//        j.tippel();
+//        j.tippel();
+    }
 }
